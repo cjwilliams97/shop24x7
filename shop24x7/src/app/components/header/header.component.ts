797b1fc7102eb4ee2e33c7c7979dc075d22b2275
Zaubawk <65273795+Zaubawk@users.noreply.github.com>
2021-12-21 09:10:15 -0800

    Logout button
    (1) Adding logout button upon user login, (2) Redirect user upon logout

--- shop24x7/src/app/components/header/header.component.ts
+++ shop24x7/src/app/components/header/header.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  userIsAtuthenticated = false;
 
-  constructor() { }
+  // userIsAtuthenticated = false;
+  private authListnerSubs: Subscription = new Subscription;
 
-  ngOnInit(): void {
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.authListnerSubs = this.userService.getAuthStatusListner().subscribe(isAuthenticated => {
+      this.userIsAtuthenticated = isAuthenticated;
+    });
+  }
+
+  onLogout(){
+    this.userService.logout();
+  }
+
+  ngOnDestroy() {
+    this.authListnerSubs.unsubscribe();
   }
 
 }
--- shop24x7/src/app/services/user.service.ts
+++ shop24x7/src/app/services/user.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { User } from '../interfaces/user';
-import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { Router } from '@angular/router';
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -16,35 +20,73 @@ export class UserService {
     interests: '',
     address: ['','','',0]
   }
-  constructor(private http : HttpClient) { }
+  //token : string = "";
+
+  private token!: string;
+
+  private authStatusListner = new Subject<boolean>();
+  private isAuthenticated = false;
+
+  constructor(private http : HttpClient, private router: Router) { }
+
+
+  getAuthStatusListner(){
+    return this.authStatusListner.asObservable();
+  }
+
+  getIsAuth(){
+    return this.isAuthenticated;
+  }
 
   getCurrentUser() {
-    var userData = JSON.parse(document.cookie).userData;
-    return userData;
+    return this.currentUser;
   }
   getToken() {
-    var token = JSON.parse(document.cookie).userToken
-    return token;
+    return this.token;
   }
   createNewUser(newUser: User){
     //add user to db (use for registering a new user)
+    this.http.post('http://localhost:3000/registerUser', newUser)
+    .subscribe(response => {
+      console.log(response);
+    });
   }
+
   signInUser(loginData : any) {
     return new Promise((resolve, reject) => {
       this.http.post('http://localhost:3000/loginUser', loginData)
       .subscribe(
         (res : any) => {
-          var CookieObj = {userToken: res.token, userData: res.userData}
-          document.cookie = JSON.stringify(CookieObj);
-
-          resolve(res)
+          this.currentUser = res.userData;
+          this.token = res.token;
+          
+          resolve(res);
+          if(this.token){
+            this.isAuthenticated = true;
+            this.authStatusListner.next(true);
+          }
+          
         },
         (error) => {
           reject(error)
           
         }
       )
-    })
+    });
     
   }
+
+  logout(){
+    this.token = null as any;
+    this.isAuthenticated = false;
+    this.authStatusListner.next(false);
+    this.router.navigate(['/']);
+  }
+ 
+
+
 }
+
+
+
+
